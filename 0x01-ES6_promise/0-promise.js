2079d68ed602a397381a27f7d9d8d13c80803616
Remember <kelvin6bolou@gmail.com>
2024-03-06 12:33:59 +0100

    a function that returns a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromApi() {
-  return new Promise((resolve, reject) => {
-    const works = true;
-    if (works) {
-      resolve('Everything works fine');
-    } else {
-      reject("There's an error");
-    }
+  return new Promise((resolve) => {
+    resolve('Everything works');
   });
 }
