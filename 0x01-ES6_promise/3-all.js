e03619517d703fef81f4cdb419d6f08184cdea00
Remember <kelvin6bolou@gmail.com>
2024-03-06 14:36:52 +0100

    try using template literals

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
     .then((values) => {
       const { body } = values[0];
       const { firstName, lastName } = values[1];
-      console.log(body, firstName, lastName);
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
