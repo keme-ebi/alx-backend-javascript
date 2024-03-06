39002ef01d46c3b57d06fe2b338f451595702074
Remember <kelvin6bolou@gmail.com>
2024-03-06 14:01:31 +0100

    called the promises inside the Promise.all() to ensure they are executed concurrently

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-const photo = uploadPhoto();
-const user = createUser();
-
 export default function handleProfileSignup() {
-  Promise.all([photo, user])
+  Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log(new Error('Signup system offline'));
