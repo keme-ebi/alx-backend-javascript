08e72a0e698a39a55c974a90e148d9fc20cb4997
Remember <kelvin6bolou@gmail.com>
2024-03-06 13:51:36 +0100

    using the Promise.all() to combine results of different promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+const photo = uploadPhoto();
+const user = createUser();
+
+export default function handleProfileSignup() {
+  Promise.all([photo, user])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
