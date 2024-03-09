625c28d9a2b5529d1bf097f049e61cc036b464d9
Remember <kelvin6bolou@gmail.com>
2024-03-09 21:22:02 +0100

    eslint fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -9,6 +9,7 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -20,6 +21,7 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
@@ -31,6 +33,7 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
+
   set students(students) {
     if (
       !Array.isArray(students) ||
