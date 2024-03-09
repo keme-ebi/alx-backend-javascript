a380a028ef722cb83b6019e6ca60a1d7c41ecee2
Remember <kelvin6bolou@gmail.com>
2024-03-09 18:51:55 +0100

    a function that calls two other functions and waits for the promises to settle, then returns an array with a structure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -13,6 +13,7 @@ export default async function handleProfileSignup(
 
   return arr.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value:
+      result.status === 'fulfilled' ? result.value : result.reason.toString(),
   }));
 }
