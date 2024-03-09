c4692da21803bfe4e6e1b4f4f48b42aaa3f4ee74
Remember <kelvin6bolou@gmail.com>
2024-03-09 21:55:56 +0100

    a class Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  // getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  //   a method that returns the attributes
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  //   a static method
+  static converPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
