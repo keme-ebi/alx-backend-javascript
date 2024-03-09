5c24b2c14b970933ee4b0cbbfc3d031a7b988a72
Remember <kelvin6bolou@gmail.com>
2024-03-09 22:03:06 +0100

    eslint fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -21,7 +21,7 @@ export default class Pricing {
   }
 
   set currency(currency) {
-    this._currency = currency;
+    if (currency instanceof Currency) this._currency = currency;
   }
 
   // a method that returns the attributes
