b30a28a1dd6ddd373553569a08ebac34965b806d
Remember <kelvin6bolou@gmail.com>
2024-03-13 09:17:58 +0100

    condition to check if the elements in the array are strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,6 +5,7 @@ export default function cleanSet(set, startString) {
 
   const string = [];
   for (const str of set) {
+    if (typeof str !== 'string') return '';
     if (str.startsWith(startString)) {
       string.push(str.substring(startString.length));
     }
