bf5c59d56a51ce57fb8ad7b6c912e18b5b4162d3
Remember <kelvin6bolou@gmail.com>
2024-03-13 21:30:23 +0100

    created a director and a teacher interface with 3 expected methods, created a director and teacher class that implements the director and teacher interface, created a function that return either a director or a teacher instance

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+// DirectorInterface
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// TeacherInterface
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// a Director class that implements DirectorInterface
+class Director {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getToWork(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+// a Teacher class that will implement TeacherInterface
+class Teacher {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: string | number): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
