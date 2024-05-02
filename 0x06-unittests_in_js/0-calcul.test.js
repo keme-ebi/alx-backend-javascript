a78deafc61ccbe5bbde57ceeb797f5d1286bca67
keme-ebi <kelvin6bolou@gmail.com>
2024-05-02 23:15:30 +0100

    tried using strictEqual

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,22 +3,22 @@ const assert = require('assert');
 
 describe('calculateNumber', function () {
   it('Tests the function with +ve numbers', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it('Tests the function with decimals', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
   it('Tests the function with -ve numbers', function () {
-    assert.equal(calculateNumber(-1.3, 5), 4);
-    assert.equal(calculateNumber(-1.3, -3.5), -4);
-    assert.equal(calculateNumber(-1, 6), 5);
+    assert.strictEqual(calculateNumber(-1.3, 5), 4);
+    assert.strictEqual(calculateNumber(-1.3, -3.5), -4);
+    assert.strictEqual(calculateNumber(-1, 6), 5);
   });
   it('Tests the function with zeros', function () {
-    assert.equal(calculateNumber(1, 0), 1);
-    assert.equal(calculateNumber(0, 0), 0);
-    assert.equal(calculateNumber(2.4, 0), 2);
-    assert.equal(calculateNumber(-3.4, 0), -3);
+    assert.strictEqual(calculateNumber(1, 0), 1);
+    assert.strictEqual(calculateNumber(0, 0), 0);
+    assert.strictEqual(calculateNumber(2.4, 0), 2);
+    assert.strictEqual(calculateNumber(-3.4, 0), -3);
   });
 });
