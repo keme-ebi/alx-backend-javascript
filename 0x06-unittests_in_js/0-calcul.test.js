f17629ff3da5cf78e067b0636546ab32cc692c1b
keme-ebi <kelvin6bolou@gmail.com>
2024-05-03 19:48:16 +0100

    reduce tests and test only relevant parts

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,20 +2,13 @@ const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
 describe('calculateNumber', function () {
-  it('Tests the function with +ve numbers', function () {
+  it('Tests the function', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('Tests the function with decimals', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-  it('Tests the function with -ve numbers', function () {
     assert.strictEqual(calculateNumber(-1.3, 5), 4);
     assert.strictEqual(calculateNumber(-1.3, -3.5), -4);
     assert.strictEqual(calculateNumber(-1, 6), 5);
   });
-  it('Tests the function with zeros', function () {
+  it('rounds the first argument', function () {
     assert.strictEqual(calculateNumber(1, 0), 1);
     assert.strictEqual(calculateNumber(0, 0), 0);
     assert.strictEqual(calculateNumber(2.4, 0), 2);
